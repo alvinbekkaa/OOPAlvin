a3021aaec869de686c04b07800470b4585ec1822
alvinBeka <alvinbekkaa@gmail.com>
2019-05-15 08:19:01 +0700

    Initial commit

---
+++ OOP - Git/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  OOP - Git
+//
+//  Created by Alvin Budikusuma on 15/05/19.
+//  Copyright © 2019 Alvin Budikusuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Initial
+    }
+
+
+}
+
